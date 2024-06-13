ed4975c28be7f9fa9276acb599193f8dade2a763
Tong LI <tongli.bioinfo@proton.me>
2024-06-13 15:03:26 +0000

    rename ngff_2_fractal. Save FOV info into the json as well

--- bin/ngff_2_fractal.py
+++ bin/ngff_2_fractal.py
@@ -9,22 +9,28 @@
 import fire
 from fractal_tasks_core.tasks.import_ome_zarr import import_ome_zarr
 import json
+from pathlib import Path
 
 
-def main(zarr:str, output_config: str, stem:str):
+def main(zarr:str, output_config: str, stem:str, overwrite:bool=True, **args):
     img_list = import_ome_zarr(
         zarr_urls=[],
         zarr_dir="./",
         zarr_name=zarr,
-        overwrite=True,
+        overwrite=overwrite,
         add_image_ROI_table=True,
+        **args
     )
     with open(output_config, "w") as f:
         json.dump(img_list, f)
 
-    for i, img in enumerate(img_list["image_list_updates"]):
-        with open(f"{stem}-{i}.json", "w") as f:
-            json.dump({"zarr_url":img["zarr_url"]}, f)
+    for i, img_md in enumerate(img_list["image_list_updates"]):
+        field = Path(img_md["zarr_url"]).stem
+        col = Path(img_md["zarr_url"]).parent.stem
+        row = Path(img_md["zarr_url"]).parent.parent.stem
+        img_md["attributes"]["field"] = field
+        with open(f"{stem}-{str(row)}-{str(col)}-{str(field)}.json", "w") as f:
+            json.dump(img_md, f)
 
 
 if __name__ == "__main__":
