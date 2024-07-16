84eac5581b968b19e8c9a171f0c958e1cc4783c9
Tong LI <tongli.bioinfo@proton.me>
2024-07-16 21:55:09 +0000

    switch to build fractal container

--- container/run.sh
+++ container/run.sh
@@ -1,3 +1,3 @@
 #Build the container with the Dockerfile in the same dir
-docker build -t bioinfotongli/hcs_analysis:latest .
-# docker build -t bioinfotongli/hcs_analysis:fractal -f Dockerfile.fractal .
+# docker build -t bioinfotongli/hcs_analysis:latest .
+docker build -t bioinfotongli/hcs_analysis:fractal .
